5355dd34f69a72ba299f3ba3acde32be51198876
ClementeSerrano <clementeserranosutil@gmail.com>
2018-11-15 18:11:00 -0300

    added resources types

---
+++ src/Resources/index.js
@@ -0,0 +1,11 @@
+import Users from "./Users";
+import Questions from "./Questions";
+import Answers from "./Answers";
+import Comments from "./Comments";
+
+export default {
+  Users,
+  Questions,
+  Answers,
+  Comments
+};
